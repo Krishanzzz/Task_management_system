8465466ad20d814bbba0fc08b2f6cbd6898ee491
Krishanzzz <156848293+Krishanzzz@users.noreply.github.com>
2025-05-28 11:57:48 +0530

    Create Login.jsx

---
+++ Components/Login/Login.jsx
@@ -0,0 +1,13 @@
+import {Header} from "./Header.jsx";
+import {LoginBody} from "./LoginBody.jsx";
+import {Footer} from "./Footer.jsx";
+
+export function Login() {
+    return (
+        <>
+            <Header/>
+            <LoginBody/>
+            <Footer/>
+        </>
+    )
+}
