1fd573f93d0e9205858adb4188f7eb8679f68983
Krishanzzz <156848293+Krishanzzz@users.noreply.github.com>
2025-05-28 12:05:34 +0530

    Create TLDashboard.jsx

---
+++ Components/TLDashBoard/TLDashboard.jsx
@@ -0,0 +1,11 @@
+import {Header} from "../../Header.jsx";
+import {TLDashboardBody} from "./TLDashboardBody.jsx";
+
+export function TLDashboard() {
+    return (
+        <div>
+            <Header/>
+            <TLDashboardBody />
+        </div>
+    )
+}
